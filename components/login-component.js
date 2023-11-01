a127542b7ce0fc13948697a84d616544b93850a4
Denis Vasilev <d89615022404@gmail.com>
2023-11-01 18:30:40 +0400

    оживил форму регистрации

--- components/login-component.js
+++ components/login-component.js
@@ -1,6 +1,6 @@
-import { login } from "../api.js";
+import { loginUser } from "../api.js";
 
-export function renderLoginComponent({ appElement, setToken, fetchTodosAndRender}) {
+export function renderLoginComponent({ appElement, setToken, fetchTodosAndRender }) {
     const appTemplate = `<div class="form">
     <h3 class="form-title">Форма авторизации</h3>
     <div class="form-row">
@@ -9,21 +9,33 @@ export function renderLoginComponent({ appElement, setToken, fetchTodosAndRender
         <br />
         <br />
         Пароль:
-        <input type="text" id="login-input" class="input" placeholder="Введите пароль"/>
+        <input type="text" id="password-input" class="input" placeholder="Введите пароль"/>
     </div>
     <br />
     <button class="button" id="login-button">Войти</button>
 </div>`;
-        appElement.innerHTML = appTemplate;
-        document.getElementById("login-button").addEventListener("click", () => {
-            login({
-                login: "admin",
-                password: "admin",
-            }
-            ).then((user) => {
-                setToken(`Bearer ${user.user.token}`);
-                fetchTodosAndRender();
-            })
-            
+    appElement.innerHTML = appTemplate;
+    document.getElementById("login-button").addEventListener("click", () => {
+        const login = document.getElementById("login-input").value;
+        const password = document.getElementById("password-input").value;
+        if (!login) {
+            alert("Введите логин...")
+            return;
+        }
+        if (!password) {
+            alert("Введите пароль...")
+            return;
+        }
+        loginUser({
+            login: login,
+            password: password,
+        }
+        ).then((user) => {
+            setToken(`Bearer ${user.user.token}`);
+            fetchTodosAndRender();
+        }).catch((error) => {
+            alert("Неверный логин или пароль");
         })
+
+    })
 }
